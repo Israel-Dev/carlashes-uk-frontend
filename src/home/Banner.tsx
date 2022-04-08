1b679019e014874d34357e01f09e45fda0a5e7a7
Israel Silva <izrael37@gmail.com>
2022-04-08 13:12:31 +0100

    :art: Replaced logo by correct image

--- src/home/Banner.tsx
+++ src/home/Banner.tsx
@@ -1,36 +1,45 @@
-import { useState } from 'react'
-import Styles from './Banner.styled'
-import coverImageFile from '../assets/Banner_01.webp'
-import logoImageFile from '../assets/logo.png'
-import Title from '../shared/Title'
+import { useState } from 'react';
+import Styles from './Banner.styled';
+import coverImageFile from '../assets/Banner_01.webp';
+import logoImageFile from '../assets/logo.png';
+import Title from '../shared/Title';
 
 const Banner = () => {
-    const [coverImage, setCoverImage] = useState(
-        {src: coverImageFile, alt: "Calashes UK"}
-    )
+    const [coverImage, setCoverImage] = useState({
+        src: coverImageFile,
+        alt: 'Calashes UK',
+    });
 
-    const [logoImage, setLogoImage] = useState(
-        {src: logoImageFile, alt: "Calrashes UK"}
-    )
-
-    // Fetch Image
+    const [logoImage, setLogoImage] = useState({
+        src: logoImageFile,
+        alt: 'Calrashes UK',
+    });
 
     return (
         <Styles className="banner-wrapper">
             <div className="banner-text-wrapper">
-                <Title 
-                    text={logoImage.src}
+                <Title
+                    text={
+                        'https://drive.google.com/uc?export=view&id=15BpofaJVctDboSVBRezIlcwSTzSXw-E6'
+                    }
                     isImage={true}
                     isWhite={true}
                     alt={coverImage.alt}
-                    />
+                />
                 <p className="banner-text-paragraph">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,  quis nostrud exercitation ullamco laborisnisi ut aliquip ex ea commodo consequat.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laborisnisi ut aliquip ex ea commodo consequat.
                 </p>
             </div>
-            <img className="banner-image" src={coverImage.src} alt={coverImage.alt} />
+            <img
+                className="banner-image"
+                src={coverImage.src}
+                alt={coverImage.alt}
+            />
         </Styles>
-    )
-}
+    );
+};
 
-export default Banner
+export default Banner;
